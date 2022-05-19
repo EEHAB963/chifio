f63c730bc7d25a9e2f23899df53413a722188dd1
karimheiba <kimoheiba@gmail.com>
2022-05-19 19:43:52 +0200

    button and slider fix

--- lib/Widget/primary_button.dart
+++ lib/Widget/primary_button.dart
@@ -8,26 +8,31 @@ class PrimaryButton extends StatelessWidget {
       {Key? key,
       required this.text,
       required this.buttonColor,
-      required this.colorText})
+      required this.colorText,
+      required this.onTap})
       : super(key: key);
   String text;
   Color buttonColor;
   Color colorText;
+  VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      width: 156,
-      height: 56,
-      child: Text(
-        text,
-        style: TextStyle(
-            color: colorText, fontSize: 17, fontWeight: FontWeight.w700),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        width: 156,
+        height: 56,
+        child: Text(
+          text,
+          style: TextStyle(
+              color: colorText, fontSize: 17, fontWeight: FontWeight.w700),
+        ),
+        decoration: BoxDecoration(
+            color: buttonColor,
+            borderRadius: BorderRadius.all(Radius.circular(25))),
       ),
-      decoration: BoxDecoration(
-          color: buttonColor,
-          borderRadius: BorderRadius.all(Radius.circular(25))),
     );
   }
 }
